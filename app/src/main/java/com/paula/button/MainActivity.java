8a6a4063977812d692efbb933565c87411773f37
Paula <paula.arthamys@gmail.com>
2024-07-30 16:07:22 -0300

    Initial commit

---
+++ app/src/main/java/com/paula/button/MainActivity.java
@@ -0,0 +1,26 @@
+package com.paula.button;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(v -> Toast.makeText(this, "Botão Clicado", Toast.LENGTH_SHORT).show());
+        
+
+    }
+}
